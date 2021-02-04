da699d04215858fb64be260a861f52cf1047f138
Rick Janusz <rickjanusz@gmail.com>
2021-02-04 12:37:08 -0600

    initializing backend repo for fullstack bootstrap

---
+++ keystone.ts
@@ -0,0 +1,31 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 365,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // Schema goes here
+  }),
+  ui: {
+    // TODO: Change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
